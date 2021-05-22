1caef08de5c2c71eca12afe2ad61ce07be55ecf3
Vinay <vinayrpatel0000@gmail.com>
2021-05-22 20:28:55 +0530

    Logic Change

--- room-calculator/src/App.js
+++ room-calculator/src/App.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import ButtonComponent from "./ButtonComponent";
 import people from "./people.JPG";
-import room from "./Rooms.JPG";
-import adult from "./adults.JPG";
-import children from "./children.JPG";
+import roomimg from "./Rooms.JPG";
+import adultimg from "./adults.JPG";
+import childrenimg from "./children.JPG";
 function App() {
   const [rooms, setRooms] = useState(1);
   const [adults, setAdults] = useState(1);
@@ -16,67 +16,71 @@ function App() {
   const [cplus, setCplus] = useState(false);
 
   const increment = (event) => {
+    let total = adults + child;
     switch (event.target.id) {
       case "Rooms":
-        if (rooms + 1 <= 5) {
-          setRooms((prevState) => prevState + 1);
-          if (adults + child < (rooms + 1) * 4) {
-            if (adults === rooms) {
-              setAdults((prevState) => prevState + 1);
-              setAminus(true);
-            }
-            setAminus(false);
-            setCminus(false);
-          }
+        //Enable room- if total < range and rooms>0
+        if (rooms + 1 > 0 && total < 4 * (rooms + 1)) {
           setRminus(false);
+        }
+        if (rooms + 1 <= 5) {
+          setRooms(rooms + 1);
+          //Disable room+ , room limit
+          if (rooms + 1 === 5) {
+            setRplus(true);
+          } //Enable  adult+ child+ as room is increased
 
-          if ((rooms + 1) * 4 >= adults) {
-            setAplus(false);
-            setCplus(false);
-          }
-          if (adults === rooms) {
-            setAminus(true);
-          }
-          if (child === 0) {
-            setCminus(true);
-          }
+          setAplus(false);
+          setCplus(false);
+        }
 
-          if (rooms + 1 === 5) setRplus(true);
-          else if (rooms + 1 < 5) setRplus(false);
+        //increase adults if total < rooms
+        if (total < rooms + 1) {
+          setAdults(adults + 1);
         }
 
         break;
       case "Adults":
+        //enable Adult- if adult>1
         if (adults + 1 > 0) {
           setAminus(false);
         }
 
-        if (adults + 1 + child === rooms * 4) {
+        //Increase adult if on range
+        if (total + 1 <= rooms * 4) {
+          setAdults(adults + 1);
+        }
+        //Disable adult+ and child+ as total === range
+        //Enable adult- and child - as total === range
+        if (total + 1 === rooms * 4) {
           setAplus(true);
           setCplus(true);
+          setAminus(false);
+          setCminus(false);
         }
-        if (adults + 1 + child <= rooms * 4) {
-          setAdults((prevState) => prevState + 1);
-        }
-
-        if (adults + 1 + child > (rooms - 1) * 4) {
-          setRminus(true);
-        }
+        //disable room- as total exceeds previous room range
+        if (total + 1 > (rooms - 1) * 4) setRminus(true);
         break;
       case "Child":
+        //enable child- if child>0
         if (child + 1 > 0) {
           setCminus(false);
         }
 
-        if (adults + 1 + child === rooms * 4) {
-          setCplus(true);
-          setAplus(true);
+        //increasing child if in range
+        if (total + 1 <= rooms * 4) {
+          setChild(child + 1);
         }
-        if (child + adults + 1 <= rooms * 4)
-          setChild((prevState) => prevState + 1);
-
-        if (adults + 1 + child > (rooms - 1) * 4) {
-          setRminus(true);
+        //disable child+ and adult+ if total === range
+        if (total + 1 === rooms * 4) {
+          setAplus(true);
+          setCplus(true);
+        }
+        //disable room- as total exceeds previous room range
+        if (total + 1 > (rooms - 1) * 4) setRminus(true);
+        //enable adult- as total > rooms and adults minimum 1
+        if (total + 1 > rooms && adults > 1) {
+          setAminus(false);
         }
 
         break;
@@ -86,60 +90,101 @@ function App() {
   };
 
   const decrement = (event) => {
+    let total = adults + child;
     switch (event.target.id) {
       case "Rooms":
-        if (rooms - 1 > 0) {
-          if (adults + child <= (rooms - 1) * 4)
-            setRooms((prevState) => prevState - 1);
-          if (adults + child > (rooms - 2) * 4) {
+        if (rooms - 1 <= 5) {
+          //disable room- if room =1
+          if (rooms - 1 === 1) {
+            setRminus(true);
+          }
+          //room in range 1-5
+          setRplus(false);
+        }
+
+        //setting rooms if rooms in range
+        if (total >= rooms - 1) {
+          setRooms(rooms - 1);
+        }
+
+        if (total > rooms - 1) {
+          adults > 1 ? setAminus(false) : setAminus(true);
+          child > 0 ? setCminus(false) : setCminus(true);
+          //deactivate room- if on room decrease total is greater than previous room max limit
+          if (total > 4 * (rooms - 2)) {
             setRminus(true);
           }
         }
-        if (adults + child <= (rooms - 1) * 4) {
-          if (adults > rooms - 1) setAminus(false);
-          if (child > 0) setCminus(false);
+        //total is equal to range
+        if (total === (rooms - 1) * 4) {
+          setAplus(true);
+          setCplus(true);
         }
+
         break;
       case "Adults":
-        if (adults - 1 > 0) {
-          if (adults - 1 === rooms) {
-            setAminus(true);
-          }
-          setAdults((prevState) => prevState - 1);
-          if (child - 1 + adults === (rooms - 1) * 4) {
-            setRminus(false);
-          }
+        if (total - 1 >= rooms && adults > 1) {
+          setAdults(adults - 1);
         }
-        if (child - 1 + adults < rooms * 4) {
-          setCplus(false);
+        //activate adult-, adult+ if adult >1
+        if (adults - 1 >= 1) {
+          setAminus(false);
           setAplus(false);
+          //deactivate adult- if adult is 1
+          if (adults - 1 === 1) setAminus(true);
         }
-        if (child === 0) {
+
+        //activate child+ on adult count decrease
+
+        if (total - 1 < 4 * rooms) {
+          setCplus(false);
+        }
+        //total is same as rooms
+        //Activate adult+, child+
+        //deactivate adult- , child-
+        if (total - 1 === rooms) {
+          setAplus(false);
+          setCplus(false);
+          setAminus(true);
           setCminus(true);
         }
+        //activating room- as total >=rooms
+        if (total - 1 >= rooms && total - 1 === 4 * (rooms - 1)) {
+          setRminus(false);
+        }
+
         break;
       case "Child":
-        if (child - 1 > 0) {
-          setChild((prevState) => prevState - 1);
-          if (child - 1 + adults === (rooms - 1) * 4) {
-            setRminus(false);
-          }
+        if (total - 1 >= rooms && child >= 1) {
+          setChild(child - 1);
         }
-        if (child - 1 === 0) {
-          setChild(0);
-          setCminus(true);
-          setAminus(false);
-          if (child - 1 + adults === (rooms - 1) * 4) {
-            setRminus(false);
-          }
-        }
-        if (child - 1 + adults < rooms * 4) {
+        //activate child-, child+ if child >=0
+        if (child - 1 >= 0) {
+          setCminus(false);
           setCplus(false);
+          //deactivate adult- if child is 0
+          if (child - 1 === 0) setCminus(true);
+        }
+
+        //activate adult+ on child count decrease
+
+        if (total - 1 < 4 * rooms) {
           setAplus(false);
         }
-        if (adults === rooms) {
+        //total is same as rooms
+        //Activate adult+, child+
+        //deactivate adult- , child-
+        if (total - 1 === rooms) {
+          setAplus(false);
+          setCplus(false);
           setAminus(true);
+          setCminus(true);
         }
+        //activating room- as total >=rooms
+        if (total - 1 >= rooms && total - 1 === 4 * (rooms - 1)) {
+          setRminus(false);
+        }
+
         break;
       default:
         break;
@@ -154,7 +199,7 @@ function App() {
       </p>
       <div className="box">
         <ButtonComponent
-          pic={room}
+          pic={roomimg}
           label="Rooms"
           value={rooms}
           increment={increment}
@@ -163,7 +208,7 @@ function App() {
           disableplus={rplus}
         />
         <ButtonComponent
-          pic={adult}
+          pic={adultimg}
           label="Adults"
           value={adults}
           increment={increment}
@@ -172,7 +217,7 @@ function App() {
           disableplus={aplus}
         />
         <ButtonComponent
-          pic={children}
+          pic={childrenimg}
           label="Child"
           value={child}
           increment={increment}
